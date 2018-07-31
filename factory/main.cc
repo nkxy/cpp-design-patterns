9d3a0d93d9ada1024036d74924cfefce6e1077dc
Nick Yang <nick.yang3721@gmail.com>
2018-07-31 17:45:02 -0400

    Added makefile & finished implementation

--- factory/main.cc
+++ factory/main.cc
@@ -3,11 +3,62 @@
 
 using namespace std;
 
-class Enemy; 
+class Student; 
 
-class University 
+class University {
+    public:
+    virtual Student *createStudent() = 0;
+};
 
+class Student {
+    public: 
+    virtual void printName() = 0;
+};
+
+class Math: public Student {
+    public:
+    void printName() override {
+        cout << "Mathie student made" << endl;
+    }
+};
+
+class Art: public Student {
+    public:
+    void printName() override {
+        cout << "Arts student made" << endl;
+    }
+};
+
+class Engineering: public Student {
+    public:
+    void printName() override {
+        cout << "Engineering student made" << endl;
+    }
+};
+
+class Waterloo: public University {
+    public:
+    Student *createStudent() override {
+        return new Math();
+    }
+};
+
+class MIT: public University {
+    public:
+    Student *createStudent() override {
+        return new Engineering();
+    }
+};
+
+class Toronto: public University {
+    public:
+    Student *createStudent() override {
+        return new Art();
+    }
+};
 
 int main() {
-
+    University *uni = new Waterloo();
+    uni->createStudent()->printName();
+    return 0;
 }
